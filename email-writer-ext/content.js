95593b314ef20ba2fa8f17c421fd362dd30aac97
Diksha Hazarika <dikshahazarika72@gmail.com>
2025-08-06 11:47:47 +0530

    Made Connection with Backend & Generated AI Reply Successfully.

--- email-writer-ext/content.js
+++ email-writer-ext/content.js
@@ -9,16 +9,31 @@ function createAiButton(){
     button.setAttribute('data-tooltip', 'Generate AI Reply');
     return button;
 }
+function getEmailContent(){
+    const selectors =[
+        '.h7',
+        '.a3s.ail',
+        '.gmail_quote',
+        '[role="presentation"]'
+    ];
+    for (const selector of selectors) {
+        const content = document.querySelector(selector);
+        if(content){
+            return content.innerText.trim();
+        }
+        return '';   
+    }
+}
 
 function findComposeToolbar(){
     const selectors =[
-        '.aDh',
         '.btC',
+        '.aDh',
         '[role="toolbar"]',
         'gU.Up'
     ];
-    for (const selector in selectors) {
-        const toolbar = document.querySelector(selector)
+    for (const selector of selectors) {
+        const toolbar = document.querySelector(selector);
         if(toolbar){
             return toolbar;
         }
@@ -31,7 +46,7 @@ function injectButton() {
     if(existingButton) existingButton.remove();
 
     const toolbar = findComposeToolbar();
-    if(toolbar){
+    if(!toolbar){
         console.log("Toolbar not found");
         return;
     }
@@ -40,7 +55,41 @@ function injectButton() {
     button.classList.add('ai-reply-button');
 
     button.addEventListener('click', async () => {
+        try {
+            button.innerHTML='Generating...'
+            button.disabled = true;
 
+            const emailContent = getEmailContent();
+            const response = await fetch('http://localhost:8080/api/email/generate', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    emailContent: emailContent,
+                    tone: "professional"
+                })
+            });
+
+            if(!response.ok){
+                throw new Error('API Request Failed');
+            }
+            const generatedReply = await response.text();
+            const composeBox = document.querySelector('[role="textbox"][g_editable="true"]');
+
+            if(composeBox){
+                composeBox.focus();
+                document.execCommand('insertText', false, generatedReply);
+            }else{
+                console.error('Compose box was not found');
+            }
+        } catch (error) {
+            console.error(error);
+            alert('Failed to generate reply');
+        }finally {
+            button.innerHTML = 'AI Reply';
+            button.disabled = false;
+        }
     });
 
     toolbar.insertBefore(button, toolbar.firstChild);
